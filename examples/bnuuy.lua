2a72e8d681c7d299133f037e9dbb36d9910cd47b
HVukman <peterslopp@googlemail.com>
2026-06-22 17:18:22 +0200

    Update bnuuy.lua

--- examples/bnuuy.lua
+++ examples/bnuuy.lua
@@ -1,43 +1,31 @@
 -- draw many bnuuys on screen 
-local ffi = require("ffi")
-ffi.cdef[[
-typedef struct { uint8_t red, green, blue, alpha; } rgba_pixel;
-]]
-
-local function image_ramp_green(n)
-  local img = ffi.new("rgba_pixel[?]", n)
-  local f = 255/(n-1)
-  for i=0,n-1 do
-    img[i].green = i*f
-    img[i].alpha = 255
-  end
-  return img
-end
-
-local function image_to_grey(img, n)
-  for i=0,n-1 do
-    local y = 0.3*img[i].red + 0.59*img[i].green + 0.11*img[i].blue
-    img[i].red = y; img[i].green = y; img[i].blue = y
-  end
-end
-
-
 function init()
-        local N = 400*400
-    local img = image_ramp_green(N)
-    for i=1,1000 do
-    image_to_grey(img, N)
+    title = "BNUUY"
+   
+    bnuuy_pos = {}
+    for i=1,100000 do
+        table.insert(bnuuy_pos, {math.random(0,screenwidth), math.random(0,screenheight)})
     end
+    palette= 0
+    load_image("bnuuy","examples/resources/bnuuy.png")
+    speed = 0.1
 end
 
 function update()
 
+    for i=1,#bnuuy_pos do
+         bnuuy_pos[i][1] = bnuuy_pos[i][1] + math.random()*math.random(-1,1)
+         bnuuy_pos[i][2] = bnuuy_pos[i][2] + math.random()*math.random(-1,1)
+    end
    
 end
 
 function draw()
     clear_background(color.RAYWHITE)
 
-   
-    
+    for i,v in ipairs(bnuuy_pos) do
+        draw_sprite( "bnuuy", 1 ,v[1],v[2])
+    end
+
+    draw_text(string.format("FPS: %i",get_fps()),20,0,25, color.YELLOW )
 end
